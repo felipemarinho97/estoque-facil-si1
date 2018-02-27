7c5f141c747b63279b859d7aa851c2f159db6f41
filipegl <filipe.lima@ccc.ufcg.edu.br>
2018-02-27 18:59:08 -0300

    .

--- src/main/java/com/ufcg/si1/config/WebSecurityConfig.java
+++ src/main/java/com/ufcg/si1/config/WebSecurityConfig.java
@@ -18,9 +18,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.
 			authorizeRequests()
+				.antMatchers("/").permitAll()
 				.anyRequest()
 				.authenticated()
-				.and().formLogin()
+				.and()
+				.formLogin()
+					.loginPage("/#!/login/")
+					.permitAll()
 				;
 				
 		http.csrf().disable();
--- src/main/java/com/ufcg/si1/controller/LoginController.java
+++ src/main/java/com/ufcg/si1/controller/LoginController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/#!")
 public class LoginController {
 	@RequestMapping(method = RequestMethod.GET, path = "/login/")
 	public String login() {
 		return "app/components/login/login-view";
+	}
+	@RequestMapping(method = RequestMethod.GET, path = "/")
+	public String home() {
+//		return "app/components/search-product/search-product-view.html";
+		return "index";
 	}
 }
